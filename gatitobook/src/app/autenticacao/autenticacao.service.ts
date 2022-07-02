c3c50be2f1f3baaf9551511ef6b9be8350be5daf
João Marcos <joaomsfreis@gmail.com>
2022-07-02 15:22:24 -0300

    Estudando Angular com Alura: boas práticas em arquiteturas e formulários

---
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,33 @@
+import { UsuarioService } from './usuario/usuario.service';
+import { Observable, tap } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AutenticacaoService {
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
+
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          console.log(res.headers);
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
+  }
+}
